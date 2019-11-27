f54009ef3d8c26f7e05ebc583d39d7939efc7603
Trammell Hudson <hudson@trmm.net>
2019-11-27 19:53:14 +0100

    efm32: split IEEE802154 parser into serialize/deserialize and add round trip tests
    Signed-off-by: Trammell Hudson <hudson@trmm.net>

--- ports/efm32/modules/IEEE802154.py
+++ ports/efm32/modules/IEEE802154.py
@@ -1,68 +1,149 @@
 # Process IEEE 802.15.4 packets and return an unpacked form of them
-# The Zigbee specific stuff should be split out into a separate file
-import CCM
 
-class IEEE802154:
-	def __init__(self, aes):
-		self.aes = aes
+FRAME_TYPE_BEACON = 0x0
+FRAME_TYPE_DATA = 0x1
+FRAME_TYPE_ACK = 0x2
+FRAME_TYPE_CMD = 0x3
+
+class Packet:
+
+	# Construct an IEEE802154 packet either from individual parts
+	# or from a byte stream off the radio passed in as data.
+	def __init__(self,
+		frame_type = 0,
+		src = None,
+		dst = None,
+		src_pan = None,
+		dst_pan = None,
+		seq = 0,
+		payload = b'',
+		ack_req = False,
+		data = None
+	):
+		if data is None:
+			self.frame_type = frame_type
+			self.ack_req = ack_req
+			self.src = src
+			self.dst = dst
+			self.src_pan = src_pan
+			self.dst_pan = dst_pan
+			self.seq = seq
+			self.payload = payload
+		else:
+			self.deserialize(data)
 
 	# parse an incoming 802.15.4 message
-	# b should be a "Packet"
-	def parse(self,b):
-		self.fcf = b.u16()
-		self.seq = b.u8()
-		self.frame_type = (self.fcf >> 0) & 0x7
+	def deserialize(self,b):
+		j = 0
+		fcf = b[j+0] << 0 | b[j+1] << 8
+		j += 2
+		self.frame_type = (fcf >> 0) & 0x7
+		self.ack_req = ((fcf >> 5) & 1) != 0
+		dst_mode = (fcf >> 10) & 0x3
+		src_mode = (fcf >> 14) & 0x3
+		self.src = None
+		self.dst = None
+		self.dst_pan = None
+		self.src_pan = None
 
-		dst_mode = (self.fcf >> 10) & 0x3
-		src_mode = (self.fcf >> 14) & 0x3
-		self.src = 0
-		self.dst = 0
-		self.dst_pan = 0
-		self.src_pan = 0
-		self.mic = None
+		self.seq = b[j]
+		j += 1
 
-		if dst_mode == 2:
-			# short destination addresses
-			self.dst_pan = b.u16()
-			self.dst = b.u16()
-		elif dst_mode == 3:
-			# long addresses
-			self.dst_pan = b.u16()
-			self.dst_addr = b.data(8)
+		if dst_mode != 0:
+			# Destination pan is always in the message
+			self.dst_pan = (b[j+0] << 0) | (b[j+1] << 8)
+			j += 2
+			if dst_mode == 2:
+				# short destination addresses
+				self.dst = b[j+0] << 0 | b[j+1] << 8
+				j += 2
+			elif dst_mode == 3:
+				# long addresses
+				self.dst = b[j:j+8]
+				j += 8
+			else:
+				throw("Unknown dst_mode %d" % (dst_mode))
 
-		if src_mode == 2:
-			# short source addressing
-			if (self.fcf >> 6) & 1:
+		if src_mode != 0:
+			if (fcf >> 6) & 1:
 				# pan compression, use the dst_pan
 				self.src_pan = self.dst_pan
 			else:
 				# pan is in the message
-				self.src_pan = b.u16()
-			self.src = b.u16()
-		elif src_mode == 3:
-			if (self.fcf >> 6) & 1:
-				# pan compression, use the dst_pan
-				self.src_pan = self.dst_pan
-			else:
-				# pan is in the message
-				self.src_pan = b.u16()
-			self.src_addr = b.data(8)
+				self.src_pan = b[j+0] << 0 | b[j+1] << 8
+				j += 2
 
-		if self.frame_type == 0:
-			self.frame_type = "BCN"
-			self.payload = b.data(b.remaining())
-		elif self.frame_type == 1:
-			self.frame_type = "DAT"
-			self.nwk_parse(b)
-		elif self.frame_type == 2:
-			self.frame_type = "ACK"
-			self.payload = b.data(b.remaining())
-		elif self.frame_type == 3:
-			self.frame_type = "CMD"
-			self.cmd_parse(b)
+			if src_mode == 2:
+				# short source addressing
+				self.src = b[j+0] << 0 | b[j+1] << 8
+				j += 2
+			elif src_mode == 3:
+				# long source addressing
+				self.src = b[j:j+8]
+				j += 8
+			else:
+				throw("Unknown src_mode %d" % (src_mode))
+
+		# the rest of the message is the payload for the next layer
+		self.payload = b[j:]
+		return self
+
+	def serialize(self):
+		hdr = bytearray()
+		hdr.append(0) # FCF will be filled in later
+		hdr.append(0)
+		hdr.append(self.seq)
+
+		fcf = self.frame_type & 0x7
+		if self.ack_req:
+			fcf |= 1 << 5 # Ack request
+
+		# Destination address mode
+		hdr.append((self.dst_pan >> 0) & 0xFF)
+		hdr.append((self.dst_pan >> 8) & 0xFF)
+		if type(self.dst) is int:
+			# short addressing, only 16-bits
+			fcf |= 0x2 << 10
+			hdr.append((self.dst >> 0) & 0xFF)
+			hdr.append((self.dst >> 8) & 0xFF)
+		elif self.dst is not None:
+			# long address, should be 8 bytes
+			if len(self.dst) != 8:
+				throw("dst address must be 8 bytes")
+			fcf |= 0x3 << 10
+			hdr.extend(self.dst)
 		else:
-			self.frame_type = "%03b" % (self.frame_type)
-			self.payload = b.data(b.remaining())
+			# no dst information? this isn't valid?
+			pass
+
+		# Source address mode; can be ommitted entirely
+		if self.src is not None:
+			if self.src_pan is None or self.src_pan == self.dst_pan:
+				fcf |= 1 << 6 # Pan ID compression
+			else:
+				hdr.append((self.src_pan >> 0) & 0xFF)
+				hdr.append((self.src_pan >> 8) & 0xFF)
+
+			if type(self.src) is int:
+				# short address, only 16-bits
+				fcf |= 0x2 << 14
+				hdr.append((self.src >> 0) & 0xFF)
+				hdr.append((self.src >> 8) & 0xFF)
+			else:
+				# long address, should be 8 bytes
+				if len(self.src) != 8:
+					throw("src address must be 8 bytes")
+				fcf |= 0x3 << 14
+				hdr.extend(self.src)
+
+		# add in the frame control field
+		hdr[0] = (fcf >> 0) & 0xFF
+		hdr[1] = (fcf >> 8) & 0xFF
+
+		hdr.extend(self.payload)
+
+		return hdr
+
 
 	# parse an IEEE802.15.4 command
 	def cmd_parse(self, b):
@@ -166,109 +247,53 @@ class IEEE802154:
 			self.payload = C
 			self.mic = 1
 
-	def show(self):
-		print(self.frame_type,
-			"fcf=%04x seq=%02x" % (self.fcf, self.seq),
-			"dst=%04x/%04x src=%04x/%04x" %
-			   (self.dst, self.dst_pan, self.src, self.src_pan),
-			self.payload
-		)
+	def __str__(self):
+		return "IEEE802154.Packet(" + ", ".join((
+			"dst=" + str(self.dst),
+			"dst_pan=" + str(self.dst_pan),
+			"src=" + str(self.src), 
+			"src_pan=" + str(self.src_pan),
+			"seq=" + str(self.seq),
+			"frame_type=" + str(self.frame_type),
+			"ack_req=" + str(self.ack_req),
+			"payload=" + str(self.payload)
+		)) + ")"
 
-
-def serialize(
-	dst,
-	pan,
-	seq,
-	frame_type,
-	payload,
-	src=None,
-	src_pan=None,
-	ack_req=False
-):
-	hdr = bytearray()
-	hdr.append(0) # FCF will be filled in later
-	hdr.append(0)
-	hdr.append(seq)
-
-	fcf = frame_type & 0x7
-	if ack_req:
-		fcf |= 1 << 5 # Ack request
-
-	# Destination address mode
-	hdr.append((pan >> 0) & 0xFF)
-	hdr.append((pan >> 8) & 0xFF)
-	if type(dst) is int:
-		# short addressing, only 16-bits
-		fcf |= 0x2 << 10
-		hdr.append((dst >> 0) & 0xFF)
-		hdr.append((dst >> 8) & 0xFF)
-	elif dst is not None:
-		# long address, should be 8 bytes
-		if len(dst) != 8:
-			throw("dst address must be 8 bytes")
-		fcf |= 0x3 << 10
-		hdr.extend(dst)
-	else:
-		# no dst information? this isn't valid?
-		pass
-
-	# Source address mode; can be ommitted entirely
-	if src is not None:
-		if src_pan is None:
-			fcf |= 1 << 6 # Pan ID compression
-		else:
-			hdr.append((src_pan >> 0) & 0xFF)
-			hdr.append((src_pan >> 8) & 0xFF)
-
-		if type(src) is int:
-			# short address, only 16-bits
-			fcf |= 0x2 << 14
-			hdr.append((src >> 0) & 0xFF)
-			hdr.append((src >> 8) & 0xFF)
-		else:
-			# long address, should be 8 bytes
-			if len(src) != 8:
-				throw("src address must be 8 bytes")
-			fcf |= 0x3 << 14
-			hdr.extend(src)
-
-	# add in the frame control field
-	hdr[0] = (fcf >> 0) & 0xFF
-	hdr[1] = (fcf >> 8) & 0xFF
-
-	hdr.extend(payload)
-
-	return hdr
 
 #from binascii import hexlify
-join_test = serialize(
-	0x0000, # dst short id
-	0x1a62, # dst pan
-	123, # seq
-	0x3, # command
-	b'\x01\x80',  # payload
-	src = b'\x58\xdf\x3e\xfe\xff\x57\xb4\x14',
-	src_pan = 0xFFFF,
-	ack_req = True
+join_test = Packet(
+	dst		= 0x0000,
+	dst_pan		= 0x1a62,
+	src		= b'\x58\xdf\x3e\xfe\xff\x57\xb4\x14',
+	src_pan		= 0xFFFF,
+	seq		= 123,
+	frame_type	= 0x3, # command
+	payload		= b'\x01\x80',
+	ack_req		= True
 )
 join_golden = bytearray(b'\x23\xc8\x7b\x62\x1a\x00\x00\xff\xff\x58\xdf\x3e\xfe\xff\x57\xb4\x14\x01\x80')
-if join_test != join_golden:
+if join_test.serialize() != join_golden:
 	print("serial join test failed:")
 	print(join_test)
 	print(join_golden)
+join_round = Packet(data=join_golden)
+if join_round.serialize() != join_golden:
+	print("join round trip failed:");
+	print(join_round)
+	print(join_golden)
 
 
-resp_test = serialize(
-	b'\x58\xdf\x3e\xfe\xff\x57\xb4\x14', # dst
-	0x1a62, # dst_pan
-	195, # seq
-	0x3, # command
-	b'\x02\x3d\x33\x00',
-	src = b'\xb1\x9d\xe8\x0b\x00\x4b\x12\x00',
-	ack_req = True
+resp_test = Packet(
+	src		= b'\xb1\x9d\xe8\x0b\x00\x4b\x12\x00',
+	dst		= b'\x58\xdf\x3e\xfe\xff\x57\xb4\x14',
+	dst_pan		= 0x1a62, # dst_pan
+	seq		= 195, # seq
+	frame_type	= 0x3, # command
+	payload		= b'\x02\x3d\x33\x00',
+	ack_req		= True
 )
 resp_golden = bytearray(b'\x63\xcc\xc3\x62\x1a\x58\xdf\x3e\xfe\xff\x57\xb4\x14\xb1\x9d\xe8\x0b\x00\x4b\x12\x00\x02\x3d\x33\x00')
-if resp_test != resp_golden:
+if resp_test.serialize() != resp_golden:
 	print("serial resp test failed:")
 	print(resp_test)
 	print(resp_golden)

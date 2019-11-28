0100bc5765b8c779ba8628516e920552d04ffa63
Trammell Hudson <hudson@trmm.net>
2019-11-28 23:17:46 +0100

    efm32: can join network now!
    Signed-off-by: Trammell Hudson <hudson@trmm.net>

--- ports/efm32/modules/Skink.py
+++ ports/efm32/modules/Skink.py
@@ -15,56 +15,89 @@ aes = AES.AES(nwk_key)
 # state to track
 
 Radio.init()
+seq = 99
+pan = 0x1a62
+mac_addr = Radio.mac()
+nwk_addr = 0xffff
+max_spins = 20000
+
 
 def loop(sniff):
 	if sniff:
 		Radio.promiscuous(1)
 
 	while True:
-		# the radio is a global from the micropython environment
-		b = Radio.rx()
-		if b is None:
-			continue
+		process_packet()
 
-		try:
-			#print("------")
-			# discard the weird bytes, not FCS, not sure what they are
-			data = b[:-2]
-			#print(data)
-			ieee = IEEE802154.IEEE802154(data=data)
-			#print(ieee)
-			if ieee.frame_type != IEEE802154.FRAME_TYPE_DATA:
-				continue
-			nwk = ZigbeeNetwork.ZigbeeNetwork(data=ieee.payload, aes=aes, validate=False)
-			ieee.payload = nwk
-			#print(nwk)
-			if nwk.frame_type != ZigbeeNetwork.FRAME_TYPE_DATA:
-				continue
-			aps = ZigbeeApplication.ZigbeeApplication(data=nwk.payload)
-			nwk.payload = aps
-			#print(aps)
-			if aps.frame_type != ZigbeeApplication.FRAME_TYPE_DATA:
-				continue
+def process_one():
+	spins = 0
+	while spins < max_spins:
+		pkt = process_packet()
+		if pkt is not None:
+			return pkt
+		spins += 1
+	return None
 
-			# join requests are "special" for now
-			if aps.cluster == 0x36:
-				continue
+def process_packet():
+	b = Radio.rx()
+	if b is None:
+		return None
 
-			zcl = ZigbeeCluster.ZigbeeCluster(data=aps.payload)
-			aps.payload = zcl
+	try:
+		#print("------")
+		# discard the weird bytes, not FCS, not sure what they are
+		data = b[:-2]
+		#print(data)
+		ieee = IEEE802154.IEEE802154(data=data)
 
-			print("%02x %04x%c %04x: cluster %04x:%02x/%d" % (
-				nwk.seq,
-				nwk.src,
-				' ' if nwk.src == ieee.src else '+', # repeater
-				nwk.dst,
-				aps.cluster,
-				zcl.command,
-				zcl.direction,
-			))
-		except:
-			print(b[:-2])
-			raise
+		# quick check for ack
+		if ieee.ack_req and \
+		(((type(ieee.dst) is bytearray) and ieee.dst == mac_addr) \
+		or ((ieee.dst is not None) and ieee.dst == nwk_addr)):
+			ack(ieee.seq)
+
+		#print(ieee)
+		if ieee.frame_type != IEEE802154.FRAME_TYPE_DATA:
+			return ieee
+		if len(ieee.payload) == 0:
+			print(ieee)
+			return ieee
+
+		nwk = ZigbeeNetwork.ZigbeeNetwork(data=ieee.payload, aes=aes, validate=False)
+		ieee.payload = nwk
+		#print(nwk)
+		if nwk.frame_type != ZigbeeNetwork.FRAME_TYPE_DATA:
+			return ieee
+
+		aps = ZigbeeApplication.ZigbeeApplication(data=nwk.payload)
+		nwk.payload = aps
+		#print(aps)
+		if aps.frame_type != ZigbeeApplication.FRAME_TYPE_DATA:
+			return ieee
+
+		# join requests are "special" for now
+		if aps.cluster == 0x36:
+			return ieee
+
+		zcl = ZigbeeCluster.ZigbeeCluster(data=aps.payload)
+		aps.payload = zcl
+
+		print("%02x %04x%c %04x: cluster %04x:%02x/%d" % (
+			nwk.seq,
+			nwk.src,
+			' ' if nwk.src == ieee.src else '+', # repeater
+			nwk.dst,
+			aps.cluster,
+			zcl.command,
+			zcl.direction,
+		))
+
+		return ieee
+	except:
+		for c in b:
+			print("%02x" % (c), end='')
+		print()
+		raise
 
 def sniff():
 	Radio.promiscuous(True)
@@ -77,21 +110,10 @@ def sniff():
 		print()
 
 
-seq = 99
-pan = 0x1a62
-mac = Radio.mac()
 beacon_packet = IEEE802154.IEEE802154(frame_type=3, seq=99, command=7, dst=0xffff, dst_pan=0xffff)
-
-def tx(msg):
-	print("-->", msg)
-	b = msg.serialize()
-	#print(b)
-	Radio.tx(b)
 
 def beacon():
 	tx(beacon_packet)
-
-max_spins = 100000
 
 def wait_packet(wait_type):
 	spins = 0
@@ -109,6 +131,20 @@ def wait_packet(wait_type):
 		return ieee
 	return None
 
+def tx(msg, wait=True, retries=3):
+	print("-->", msg)
+	b = msg.serialize()
+	for i in range(retries):
+		Radio.tx(b)
+		if not wait or not msg.ack_req:
+			return True
+
+		ack = wait_packet(IEEE802154.FRAME_TYPE_ACK)
+		if ack is not None and ack.seq == msg.seq:
+			return True
+		print("<-- RETRY")
+	return False
+
 def discover_pan():
 	for i in range(10):
 		beacon()
@@ -118,9 +154,6 @@ def discover_pan():
 	return None
 	
 
-def wait_ack():
-	return wait_packet(IEEE802154.FRAME_TYPE_ACK) is not None
-
 def leave():
 	global seq
 	tx(IEEE802154.IEEE802154(
@@ -128,34 +161,55 @@ def leave():
 		seq=seq,
 		dst=0x0000,
 		dst_pan=pan,
-		src=0x333d,
+		src=nwk_addr,
 		payload=ZigbeeNetwork.ZigbeeNetwork(
+			aes=aes,
 			frame_type=ZigbeeNetwork.FRAME_TYPE_CMD,
 			version=2,
 			radius=1,
 			seq=12,
 			dst=0xfffd,
-			src=mac,
+			src=nwk_addr,
+			ext_src=mac_addr,
 			discover_route=0,
 			security=1,
-			sec_seq=bytearray(b';\x00\x00\x00'),
+			sec_seq=bytearray(b'\x00\x00\x00\x00'),
 			sec_key=1,
 			sec_key_seq=0,
-			ext_src=mac,
 			payload=bytearray(b'\x04\x00')
 		),
 	))
 	seq += 1
 
-def ack(seq):
+def ack(ack_seq):
 	tx(IEEE802154.IEEE802154(
 		frame_type	= IEEE802154.FRAME_TYPE_ACK,
-		seq		= seq
+		seq		= ack_seq
+	))
+
+def data_request(src=None):
+	global seq
+	this_seq = seq
+	seq += 1
+
+	if src is None:
+		src = nwk_addr
+	
+	return tx(IEEE802154.IEEE802154(
+		frame_type	= IEEE802154.FRAME_TYPE_CMD,
+		command		= IEEE802154.COMMAND_DATA_REQUEST,
+		ack_req		= True,
+		seq		= this_seq,
+		src		= src,
+		dst		= 0x0000,
+		dst_pan		= pan,
+		payload		= b'',
 	))
 
 def join():
 	global seq
-	global nwk
+	global nwk_addr
+	global pan
 
 	Radio.promiscuous(1)
 
@@ -166,61 +220,54 @@ def join():
 
 	print("PAN=0x%04x" % (pan))
 
-	tx(IEEE802154.IEEE802154(
+	# Send the join request
+	if not tx(IEEE802154.IEEE802154(
 		frame_type	= IEEE802154.FRAME_TYPE_CMD,
 		command		= IEEE802154.COMMAND_JOIN_REQUEST,
 		seq		= seq,
-		src		= mac,
+		src		= mac_addr,
 		src_pan		= 0xFFFF,
 		dst		= 0x0000,
 		dst_pan		= pan,
 		ack_req		= True,
 		payload		= b'\x80', # Battery powered, please allocate address
-	))
-	seq = seq + 1
-
-	if not wait_ack():
-		print("JOIN No ack received?")
+	)):
+		print("JOIN_REQUEST: no ack")
 		return False
+	seq += 1
 
-	print("JOIN ack received")
-	#for i in range(10000):
-	#	pass
-
-	tx(IEEE802154.IEEE802154(
-		frame_type	= IEEE802154.FRAME_TYPE_CMD,
-		command		= IEEE802154.COMMAND_DATA_REQUEST,
-		ack_req		= True,
-		seq		= seq,
-		src		= mac,
-		dst		= 0x0000,
-		dst_pan		= pan,
-		payload		= b'',
-	))
-	seq = seq + 1
-
-	if not wait_ack():
-		print("DATA No ack received?")
+	if not data_request(src=mac_addr):
+		print("DATA_REQUEST: long no ack")
 		return False
-
-	print("DATA ack received")
 
 	assoc = wait_packet(IEEE802154.FRAME_TYPE_CMD)
 	if assoc is None:
 		print("ASSOCIATE RESPONSE not received?")
 		return False
 	ack(assoc.seq)
+
 	if assoc.command != IEEE802154.COMMAND_JOIN_RESPONSE:
 		print("Not an associate response?")
 		return False
-	nwk = (assoc.payload[0] << 0) | (assoc.payload[1] << 8)
+
+	nwk_addr = (assoc.payload[0] << 0) | (assoc.payload[1] << 8)
 	status = assoc.payload[2]
 
-	print("New network address 0x%04x status %d" % (nwk, status))
-	Radio.address(nwk, pan)
-	Radio.promiscuous(0)
+	print("New network address 0x%04x status %d" % (nwk_addr, status))
+	#Radio.promiscuous(0)
+	#Radio.address(nwk_addr, pan)
 
+	#if not data_request():
+		#print("DATA_REQUEST: short no ack")
+
+	# try to get the network key
+	#for i in range(10):
+	print("---- Sending request ----")
+	if not data_request():
+		print("DATA_REQUEST: short no ack")
+	process_one()
+		
 	#print("Running loop")
-	#loop(1)
+	#loop(0)
 
 	return True

22de0d2532b8457917542709238b7e14ea369ed3
Trammell hudson <hudson@trmm.net>
2019-12-26 16:29:11 +0100

    efm32: added chip_erase and other SPI flash functions to flashbdev

--- ports/efm32/modules/__init__.py
+++ ports/efm32/modules/__init__.py
@@ -11,13 +11,14 @@ def mount():
 		# Create a VFS mapping of the block device
 		vfs = uos.VfsLfs2(bdev)
 	except:
+		uos.VfsLfs2.umount(bdev)
 		print("""
 lfs2 failed. to format the flash run these commands:
 
-		from flashbdev import bdev
-		import uos
-		uos.VfsLfs2.mkfs(bdev)
-		uos.mount(uos.VfsLfs2(bdev), '/')
+from flashbdev import bdev
+import uos
+uos.VfsLfs2.mkfs(bdev)
+uos.mount(uos.VfsLfs2(bdev), '/')
 """)
 		return
 
--- ports/efm32/modules/flashbdev.py
+++ ports/efm32/modules/flashbdev.py
@@ -1,5 +1,6 @@
 from machine import Pin, SPI, SPIFlash
 from ubinascii import hexlify
+import uos
 
 class FlashBdev:
 
@@ -23,11 +24,7 @@ class FlashBdev:
 	# Ikea matched the same pinout when they laid out the On/Off switch
 	self.flash = SPIFlash(self.cs, self.spi)
 
-	# try to read the chip id
-	self.cs.off()
-	self.spi.write(b'\x9f')
-	print("SPI device ID:", hexlify(self.spi.read(3)))
-	self.cs.on()
+	print("SPI device ID:", self.rdid())
 
 
     def readblocks(self, n, buf, off=0):
@@ -35,7 +32,7 @@ class FlashBdev:
         self.flash.read(((n + self.START_SEC) << self.SEC_SHIFT) + off, buf)
 
     def writeblocks(self, n, buf, off=None):
-        #print("writeblocks(%s, %x(%d), %d)" % (n, id(buf), len(buf), off))
+        print("writeblocks(%s, %x(%d), %d)" % (n, id(buf), len(buf), off))
         #assert len(buf) <= self.SEC_SIZE, len(buf)
         if off is None:
             self.flash.erase(n + self.START_SEC)
@@ -52,5 +49,84 @@ class FlashBdev:
             self.flash.erase(arg + self.START_SEC)
             return 0
 
-size = 0x100000
+    def all_ff(self, buf):
+        for x in buf:
+            if x != 0xFF:
+                return False
+        return True
+
+    def format(self, key = 0):
+        if key != 0xDEAD:
+		print("Wrong key, pass in 0xDEAD to be sure")
+		return
+
+	try:
+		print("UNMOUNTING FLASH")
+		uos.umount("/")
+	except:
+		pass
+
+        print("ERASING THE FLASH")
+	self.chip_erase()
+
+	print("\nMAKING LFS2 FILESYSTEM")
+	uos.VfsLfs2.mkfs(self)
+
+	print("MOUNTING LFS2 FILESYSTEM")
+	uos.mount(uos.VfsLfs2(self), "/")
+
+    # try to read the chip id
+    def rdid(self):
+	self.cs.off()
+	self.spi.write(b'\x9f')
+	jdec_id = hexlify(self.spi.read(3))
+	self.cs.on()
+	return jdec_id
+
+    def _wren(self):
+	self.cs.off()
+	self.spi.write(b'\x06')
+	self.cs.on()
+
+    def wren(self):
+        for i in range(8):
+		self._wren()
+                sr = self.sr()
+                if sr & 2:
+			return True
+	print("WREN never set?")
+	return False
+
+    def chip_erase(self):
+	if not self.wren():
+		return
+	self.cs.off()
+	self.spi.write(b'\xc7')
+	self.cs.on()
+
+	if (self.sr() & 1) == 0:
+		print("WIP not set?")
+
+	for i in range(10000):
+		if (self.sr() & 1) == 0:
+			break;
+		if i % 1000 == 0:
+			print(".", end='')
+
+	print("DONE")
+
+    def sr(self):
+	self.cs.off()
+	self.spi.write(b'\x05')
+        x = self.spi.read(1)
+	self.cs.on()
+        return x[0]
+	
+    # helper to read from the flash and return it
+    def read(self, addr, length):
+	b = bytearray(length)
+	self.readblocks(addr >> self.SEC_SHIFT, b, off=addr & (self.SEC_SIZE-1))
+	return b
+
+size = 0x100000 >> 12
 bdev = FlashBdev(size) # could reserve space

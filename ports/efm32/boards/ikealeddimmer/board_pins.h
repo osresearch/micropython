a24000076cc5273c33cc71f7168d246763003865
Trammell hudson <hudson@trmm.net>
2020-02-27 22:40:31 +0100

    efm32: remove duplicated ikea boards
    Signed-off-by: Trammell hudson <hudson@trmm.net>

--- ports/efm32/boards/ikealeddimmer/board_pins.h
+++
@@ -1,7 +0,0 @@
-#define USART_TX_PORT gpioPortC
-#define USART_TX_PIN 10
-#define USART_TX_LOCATION 15 // ? found in af_pins.h somehow
-#define USART_RX_PORT gpioPortC
-#define USART_RX_PIN 11
-#define USART_RX_LOCATION 15 // ?
-
--- ports/efm32/boards/ikeaonoff/mpconfigboard.h
+++
@@ -1,3 +0,0 @@
-#define MICROPY_HW_BOARD_NAME "gecko"
-#define MICROPY_HW_MCU_NAME "efr32mg1p"
-

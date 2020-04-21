c234259214ecd25d6a87961398ea4db0cd375ba5
Trammell hudson <hudson@trmm.net>
2020-04-21 14:58:38 +0200

    airbreak: generate a single output file

--- ports/stm32/boards/AIRBREAK/mpconfigboard.mk
+++ ports/stm32/boards/AIRBREAK/mpconfigboard.mk
@@ -2,6 +2,7 @@ MCU_SERIES = f4
 CMSIS_MCU = STM32F405xx
 AF_FILE = boards/stm32f405_af.csv
 LD_FILES = boards/stm32f405.ld boards/common_ifs.ld
-TEXT0_ADDR = 0x08000000
-TEXT1_ADDR = 0x08020000
+# should generate one file, not two
+#TEXT_ADDR = 0x08000000
+#TEXT1_ADDR = 0x08020000
 CFLAGS += -g
